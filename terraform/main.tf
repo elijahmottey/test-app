174686c768b6c601212117fa3f1b8d1b983a3b68
Elijah Mottey <elijahmottey5@gmail.com>
2025-10-27 13:25:15 +0000

    fixed block xml  in browser

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,24 +35,23 @@ resource "aws_s3_bucket_public_access_block" "blockaccess" {
 }
 
 resource "aws_s3_bucket_policy" "s3_static_website_policy" {
-    bucket = aws_s3_bucket.static-site.id
-   policy = jsonencode({
-  "Version" : "2012-10-17",
-  "Id"      : "PolicyForPublicWebsiteContent",
-  "Statement" : [
-    {
-      "Sid"       : "PublicReadGetObject",
-      "Effect"    : "Allow",
-      "Principal" : "*",
-      "Action"    : "s3:GetObject",
-      "Resource"  : "arn:aws:s3:::${aws_s3_bucket.static-site.bucket}/*"
-    }
-  ]
-})
+  bucket = aws_s3_bucket.static-site.bucket
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "PolicyForPublicWebsiteContent"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.static-site.bucket}/*"
+      }
+    ]
+  })
 
-    depends_on = [ aws_s3_bucket_public_access_block.blockaccess ]
-
-  
+  depends_on = [aws_s3_bucket_public_access_block.blockaccess]
 }
+
 
