0cf892d598ce7e0b021885180bafaba836733fe9
Elijah Mottey <elijahmottey5@gmail.com>
2025-10-27 12:45:41 +0000

    fixed bucket name with  random id

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 
 resource "aws_s3_bucket" "static-site" {
-    bucket = var.bucket_name
-
+  bucket = "${var.bucket_name}-${random_id.suffix.hex}"
     
   tags = {
     Name        = "StaticSiteBucket"
