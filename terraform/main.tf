7056f16f492b984ca858eb8176630c053ecf3aab
Elijah Mottey <elijahmottey5@gmail.com>
2025-10-27 13:18:50 +0000

    fixed policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,21 +36,20 @@ resource "aws_s3_bucket_public_access_block" "blockaccess" {
 
 resource "aws_s3_bucket_policy" "s3_static_website_policy" {
     bucket = aws_s3_bucket.static-site.id
-    policy = jsondecode(
-        {
-            "Version": "2012-10-17",
-            "Id": "PolicyForPublicWebsiteContent",
-            "Statement": [          
-                {
-                    "Sid": "PublicReadGetObject",
-                    "Effect": "Allow",
-                    "Principal": "*",
-                    "Action": "s3:GetObject",
-                    "Resource": "arn:aws:s3:::${aws_s3_bucket.static-site.bucket}/*"
-                }
-            ]   
-        }
-    )
+   policy = jsonencode({
+  "Version" : "2012-10-17",
+  "Id"      : "PolicyForPublicWebsiteContent",
+  "Statement" : [
+    {
+      "Sid"       : "PublicReadGetObject",
+      "Effect"    : "Allow",
+      "Principal" : "*",
+      "Action"    : "s3:GetObject",
+      "Resource"  : "arn:aws:s3:::${aws_s3_bucket.static-site.bucket}/*"
+    }
+  ]
+})
+
 
     depends_on = [ aws_s3_bucket_public_access_block.blockaccess ]
 
