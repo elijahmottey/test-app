99c1fd1ce2c5cedd8d30e14b7ae8dd770ca858be
Elijah Mottey <elijahmottey5@gmail.com>
2025-10-28 12:43:42 +0000

    fixed env errors

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,12 +7,6 @@ terraform {
 
   backend "s3" {
 
-  bucket = "${var.lock_bucket_name}"
-  region = "${var.region}"
-  key    = "lock/terraform.tfid/terraform.tfstate"
-  encrypt = true
-  dynamodb_table = "${var.dynamodb_table_name}"
-
 }
 
 }
